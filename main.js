2b471ed1ee98bfc7654bd3ca502fc1ada8759418
Jose A. Tolentino <61167951+jatolentino@users.noreply.github.com>
2024-01-22 17:23:58 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,51 @@
+
+const owner = ""; // your Github User
+const token = ""; // your token, read the readme for more info
+let pdfFile = null;
+let theFileData  = null;
+
+function uploadFile(file, fileData, repo, folder) {
+    return fetch(
+      `https://api.github.com/repos/${owner}/${repo}/contents/${folder}/${file.name}`,
+      {
+        method: "PUT",
+        headers: {
+          Accept: "application/vnd.github+json",
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({
+          message: "upload file from api",
+          content: fileData
+        })
+      }
+    ).then((res) => res.json());
+  }
+
+ 
+function uploadTasks() {
+    const selectedfile = document.getElementById('fileID')
+    pdfFile = selectedfile.files[0];
+    const thefileReader = new FileReader();
+    thefileReader.onload = () => {
+        const srcData = thefileReader.result;
+        theFileData = srcData.split(',')[1]
+    };
+    thefileReader.readAsDataURL(pdfFile);
+    //console.log(pdfFile)
+}
+  
+
+function fillForm(){
+    //console.log(theFileData)
+    
+    const repoName = document.getElementById('repoName').value;
+    const folderName= document.getElementById('folderName').value;
+    if(repoName !== '' && folderName !== ''){
+        uploadFile(pdfFile, theFileData, repoName, folderName)}
+    else{
+        alert('Please complete the fields')
+    }
+
+}
+  
+   
